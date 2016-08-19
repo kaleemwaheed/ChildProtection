4156db5f3d694f1a1c8ee0f2e31b228f1d027d06
kaleemwaheed <kingkaleem43@gmail.com>
2016-08-19 21:48:49 +0500

    second 9 add Create report

---
+++ UIChildProtection/UIChildProtection/Areas/Common/Controllers/SubmitReportController.cs
@@ -0,0 +1,22 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UIChildProtection.Areas.Common.Controllers
+{
+    public class SubmitReportController : Controller
+    {
+        // GET: Common/SubmitReport
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create(tbl_ChildInfo obj)
+        {
+            return View();
+        }
+    }
+}
